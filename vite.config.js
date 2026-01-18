4ebb036c965fbe7c28f2e8023d825bbd1a45a2cc
blagich <slimclick@gmail.com.com>
2026-01-19 02:41:52 +0300

    fix vite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/", // ← обязательно для Netlify
+  build: {
+    outDir: "dist", // по умолчанию, но лучше явно
+    assetsDir: "assets",
+    sourcemap: true, // полезно для отладки
+  },
+  server: {
+    port: 3000,
+  },
+});
+ы;
